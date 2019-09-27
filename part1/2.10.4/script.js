8f93e4f18107e23959349c86eb3d71522d06956b
Andrey Demidkin <creator051@yandex.ru>
2019-09-27 09:01:00 +0300

    n

---
+++ part1/2.10.4/script.js
@@ -0,0 +1,13 @@
+// Перепишите if с использованием условного оператора '?':
+
+// let result;
+
+// if (a + b < 4) {
+//   result = 'Мало';
+// } else {
+//   result = 'Много';
+// }
+let a = 2;
+let b = 2;
+let result = (a + b < 4) ? 'Мало' : 'Много';
+console.log(result);

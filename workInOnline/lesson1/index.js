cbac9fa6b580c3da4bc7bcbf03c8da9c0b29efd9
Andrey Demidkin <creator051@yandex.ru>
2019-09-30 23:19:18 +0300

    more ex

--- workInOnline/lesson1/index.js
+++ workInOnline/lesson1/index.js
@@ -1,3 +1,6 @@
-const div = document.querySelector('div');
+// const div = document.querySelector('div');
 
-console.log(div.innerHTML);
+// console.log(div.innerHTML);
+debugger;
+console.log(`age - ${age}`);
+console.log(`age2 - ${age2}`);
---
+++ workInOnline/lesson1/script.js
@@ -0,0 +1,4 @@
+console.log(`age - ${age}`);
+console.log(`age2 - ${age2}`);
+var age = 33;
+let age2 = 34;

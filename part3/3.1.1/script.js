7ca57e636fe805f056d043b66a85987ade90b0b3
Andrey Demidkin <creator051@yandex.ru>
2019-12-20 14:29:40 +0300

    some fix

--- part3/3.1.1/script.js
+++ part3/3.1.1/script.js
@@ -2,37 +2,29 @@ const PATH = 'https://api.github.com/users/';
 
 const users = ['Numal-One', 'Owll', 'Owlllz'];
 
-function getUsers(users){
-  let result = [];
-  result = users.map(async (user)=>{
-    console.log( user)
+async function getUsers(users){
+  let output = [];
+  output = users.map(async (user)=>{
+    // console.log(user);
     let response = await fetch(`${PATH}${user}`);
+    console.log(`ok - ${response.ok}`);
+    if (!response.ok || response.status !== 200) {
+      return null;
+    }
     let userJson = await response.json();
-    console.log(userJson);
+    // console.log(userJson);
     return userJson;
   });
-  
- return result;
-}
-
-async function getUsers(users){
-  let result = [];
-  return Promise.all(users.map(async (user)=>{
-    
-    let respons = fetch(`${PATH}${user}`)
-    .then(
-      function(response){
-      let res = response.json();
-      console.log(res);
-      return res;
-    });
-   
-    
-  }));
-  
- 
+  console.log(`output - ${output}`);
+  let result = await Promise.all(output);
+  return result;
 }
 
 
-let a = getUsers(users);
-console.log(a)
+(async function (){
+  let a;
+  await getUsers(users).then((val) => {
+    return a = val;
+  });
+  console.log(a);
+})();
